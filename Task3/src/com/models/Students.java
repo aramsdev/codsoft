37f43a7d58a77c5efd8b00df1c606844215e95b8
aramsdev <78046280+aramsdev@users.noreply.github.com>
2023-07-12 19:41:26 -0600

    Add files via upload

---
+++ Task3/src/com/models/Students.java
@@ -0,0 +1,50 @@
+package com.models;
+
+public class Students {
+    private String firstName;
+    private String lastName;
+    private String grade;
+    private String rollNumber;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}

56a0c5e1aa160b703baf0408bf56fc71d960675d
aramsdev <78046280+aramsdev@users.noreply.github.com>
2023-07-21 20:56:18 -0600

    Add files via upload

---
+++ Task4/src/com/exceptions/Exceptions.java
@@ -0,0 +1,10 @@
+package com.exceptions;
+import java.io.IOException;
+
+public class Exceptions extends IOException{
+    public Exceptions(){}
+    
+    public Exceptions(String mess){
+        super(mess);
+    }
+}

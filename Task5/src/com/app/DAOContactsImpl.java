19964e3b200e47390152e882f8d443afa5d422d5
aramsdev <78046280+aramsdev@users.noreply.github.com>
2023-07-25 11:28:58 -0600

    Add files via upload

--- Task5/src/com/app/DAOContactsImpl.java
+++ Task5/src/com/app/DAOContactsImpl.java
@@ -6,6 +6,7 @@ import com.models.Contact;
 import java.util.ArrayList;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DAOContactsImpl extends database implements DAOContacts{
     
@@ -59,12 +60,48 @@ public class DAOContactsImpl extends database implements DAOContacts{
     }
 
     @Override
-    public Contact searchContact(String name) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ArrayList<Contact> searchContact(String name) throws Exception {
+        ArrayList<Contact> contacts = new ArrayList();
+        try{
+            this.Connect();
+            query = "SELECT * FROM contacts WHERE name LIKE '%" + name + "%';";
+            PreparedStatement st = this.connection.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+            
+            while(rs.next()){
+                Contact contact = new Contact();
+                contact.setId(rs.getInt("id"));
+                contact.setName(rs.getString("name"));
+                contact.setEmail(rs.getString("email"));
+                contact.setPhone(rs.getString("phone"));
+                contact.setCompany(rs.getString("company"));
+                contacts.add(contact);
+            }
+            rs.close();
+            st.close();
+        } catch(Exception e){
+            System.out.println(e);
+        } finally {
+            this.Close();
+        }
+        return contacts;
     }
 
     @Override
     public void deleteContact(int id) throws Exception {
+        try{
+            this.Connect();
+            query = "DELETE FROM contacts WHERE id = "+id+";";
+            PreparedStatement st = this.connection.prepareStatement(query);
+            st.executeUpdate();
+            st.close();
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println(e);
+        }
+    }
+    
+    @Override
+    public void editContact(int id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
